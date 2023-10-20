2ea56ee4ce39f926ae3c7c2c5b9d95d9eb51f88f
GG-Afterdeath <maicoljosue@outlook.com>
2023-10-20 10:21:04 -0500

    Se crea data class message y MessageCard

--- app/src/main/java/co/edu/sena/app2687365/MainActivity.kt
+++ app/src/main/java/co/edu/sena/app2687365/MainActivity.kt
@@ -3,41 +3,34 @@ package co.edu.sena.app2687365
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import co.edu.sena.app2687365.ui.theme.App2687365Theme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      App2687365Theme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Kotlin!!")
-        }
-      }
-    }
-  }
-}
+      MessageCard(Message("Android", "Jetpack Compose"))
+    }// setContent
+  }// onCreate
+}// Main Activity
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Text(
-    text = "Hello $name!",
-    modifier = modifier
-  )
+fun MessageCard(msg: Message) {
+  Column {
+    Text(text = msg.author)
+    Text(text = msg.body)
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-  App2687365Theme {
-    Greeting("Kotlin!!")
-  }
+fun PreviewMessageCard() {
+  MessageCard(
+    msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+  )
 }
